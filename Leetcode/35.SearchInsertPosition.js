b4f24b7272d1e6531d8665376ebbddcf331e3c50
NAGARRO\eishtamittal <eishtamittal@gmail.com>
2021-10-15 21:10:03 +0530

    Binary search, BFS and DP

--- Leetcode/35.SearchInsertPosition.js
+++ Leetcode/35.SearchInsertPosition.js
@@ -1,32 +1,28 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
+// Link : https://www.youtube.com/watch?v=0A40XJH_VvE
 // var searchInsert = function(nums, target) {
 //     if(nums.length<=0) return;
 //     let m= Math.trunc(nums.length/2);
 //     if(target === nums[m])return m;
 //     else if(target< nums[m])searchInsert(nums.slice(0,m), target);
 //     else searchInsert(nums.slice(m+1), target)
-    
+
 // };
-var searchInsert = function(nums, target) {
+var searchInsert = function (nums, target) {
     const middle = Math.floor(nums.length / 2);
-       let offset = 0;
-       let result = 0;
-       if(target > nums[middle]){
-           result = searchInsert(nums.slice(middle + 1, nums.length), target);
-           offset = middle + 1;
-       }
-       else if(target < nums[middle]){
-           result = searchInsert(nums.slice(0, middle), target);
-       }
-       else if(target === nums[middle]){
-           result = middle;
-       }
-       else{
-           result = 0;
-       }
-       return result + offset;
-   };
+    let offset = 0;
+    let result = 0;
+    if (target > nums[middle]) {
+        result = searchInsert(nums.slice(middle + 1, nums.length), target);
+        offset = middle + 1;
+    }
+    else if (target < nums[middle]) {
+        result = searchInsert(nums.slice(0, middle), target);
+    }
+    else if (target === nums[middle]) {
+        result = middle;
+    }
+    else {
+        result = 0;
+    }
+    return result + offset;
+};
--- Leetcode/69.sqrt.js
+++ Leetcode/69.sqrt.js
@@ -4,19 +4,19 @@
 
 // Input: x = 4
 // Output: 2
+// Link : https://www.youtube.com/watch?v=VYtEKhxKd1Q
 
-
-var mySqrt = function(x) {
+var mySqrt = function (x) {
     let left = 1;
     let right = x;
-    if(x<2)return x;
-    while (left < right){
-        let mid =left+ Math.floor((right-left)/2);
-        
-        if(mid * mid === x) return mid;
-        else if(mid * mid > x) right= mid;
-        else if(mid * mid < x) left = mid+1;
+    if (x < 2) return x;
+    while (left < right) {
+        let mid = left + Math.floor((right - left) / 2);
+
+        if (mid * mid === x) return mid;
+        else if (mid * mid > x) right = mid;
+        else if (mid * mid < x) left = mid + 1;
     }
-    return left-1;
+    return left - 1;
 };
 
