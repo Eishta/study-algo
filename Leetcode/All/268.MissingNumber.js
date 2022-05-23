6bb575b4e46d6b9ab305fd6dda127ab79e9f2e8f
Eishta <eishtamittal@gmail.com>
2022-05-23 11:34:56 +0530

    268. Missing number

--- Leetcode/All/268.MissingNumber.js
+++ Leetcode/All/268.MissingNumber.js
@@ -5,18 +5,28 @@
 
 
 // method 1
-var missingNumber = function(nums) {
-    let n = nums.length;
-    let sum = 0;
-    for(let i = 0 ;i<n;i++ ){
-      sum = sum + i + 1-nums[i]
-}
-return sum;
+var missingNumber = function (nums) {
+  let n = nums.length;
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    sum = sum + i + 1 - nums[i]
+  }
+  return sum;
 };
 
 // method 2
-var missingNumber = function(nums) {
+var missingNumber = function (nums) {
 
-    return nums.reduce((sum,ele,i)=> sum + i+ 1 -ele, 0);
-     
- };
+  return nums.reduce((sum, ele, i) => sum + i + 1 - ele, 0);
+
+};
+
+// method 3
+var missingNumber = function (nums) {
+  let sum = 0;
+  for (let i = 0; i <= nums.length; i++) {
+    nums[i] && (sum -= nums[i]);
+    sum += i;
+  }
+  return sum;
+};
