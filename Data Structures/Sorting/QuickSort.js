578b6482ab29cc1a1b55ed7fa8fdad57ec8dced3
Eishta <eishtamittal@gmail.com>
2022-05-26 15:09:36 +0530

    Quick Sort

--- Data Structures/Sorting/QuickSort.js
+++ Data Structures/Sorting/QuickSort.js
@@ -35,7 +35,12 @@
 /** SPACE COMPLEXITY
  *         O(1) as no extra DS is used 
  */
-
+/**
+ * The key process in quickSort is partition().
+ *  Target of partitions is, given an array and an element x of array as pivot,
+ *  put x at its correct position in sorted array and put all smaller elements (smaller than x) before x,
+ *  and put all greater elements (greater than x) after x. All this should be done in linear time.
+ */
 let quicksort = (A, start, end) => {
     if (start >= end) return
     let p_index = partition(A, start, end);
