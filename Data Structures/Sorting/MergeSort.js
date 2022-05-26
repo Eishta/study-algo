ab5b0bd5a859955738dcf37c7e7f6687df446b8e
Eishta <eishtamittal@gmail.com>
2022-05-26 14:58:24 +0530

    merge sort

--- Data Structures/Sorting/MergeSort.js
+++ Data Structures/Sorting/MergeSort.js
@@ -1,4 +1,4 @@
-let merge = (arr, start, mid, end) => {
+let mergeWithSplice = (arr, start, mid, end) => {
     let i = start, j = mid + 1, k = 0;
     let temp = [];
     while (i <= mid && j <= end) {
@@ -21,6 +21,38 @@ let merge = (arr, start, mid, end) => {
     }
     arr.splice(start, end - start + 1, ...temp)
 }
+let merge = (a, l, mid, r) => {
+    let n1 = mid - l + 1;
+    let n2 = r - mid;
+    let L = Array(n1);
+    let R = Array(n2);
+    let i = 0, j = 0;
+
+    while (i < n1) {
+        L[i] = a[l + i];
+        i++
+    }
+    while (j < n2) {
+        R[j] = a[mid + 1 + j];
+        j++
+    }
+    i = 0, j = 0;
+    let k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            a[k++] = L[i++];
+        }
+        else {
+            a[k++] = R[j++];
+        }
+    }
+    while (i < n1) {
+        a[k++] = L[i++];
+    }
+    while (j < n2) {
+        a[k++] = R[j++];
+    }
+}
 let mergeSort = (arr, start, end) => {
     if (start < end) {
         let mid = start + parseInt((end - start) / 2);
@@ -29,3 +61,8 @@ let mergeSort = (arr, start, end) => {
         merge(arr, start, mid, end)
     }
 }
+
+
+// let a=[3,2,6,3,1,2]
+// mergeSort(a, 0, a.length-1)
+// (6) [1, 2, 2, 3, 3, 6]
